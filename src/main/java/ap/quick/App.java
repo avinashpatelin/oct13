46f51751354c3a30e3eabf5f2415dcec02a75372
Gitea <gitea@fake.local>
2023-10-13 06:54:50 +0000

    ref

---
+++ src/main/java/ap/quick/App.java
@@ -0,0 +1,13 @@
+package ap.quick;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
